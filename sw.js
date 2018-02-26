f3e4a96e35a03dd9b755eb98a53dd32e197498e6
Marshal <marshal.wu@qq.com>
2018-02-26 17:02:51 +0800

    dd

--- sw.js
+++ sw.js
@@ -2,3 +2,7 @@ self.addEventListener("fetch", function (event) {
     // console.log(event.request)
     console.log('===>>get fetch event !! and update ~~')
 })
+
+self.addEventListener('install', event => {
+    console.log('>>>service worker install ')
+})
